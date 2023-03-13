43dbd61c29fbb7a955f89584feb49aeca9fcf781
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-03-13 13:57:14 +0100

    start lesson

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,8 @@ function App(props: AppProps) {
             <Header/>
             <Nav/>
             <div className="app-wrapper-content">
-                {/*<Dialogs/>*/}
-                {/*<Profile/>*/}
+                {/*<Dialogs messagesData={props.state.messagesPage.messagesData}    dialogsData={props.state.messagesPage.dialogsData}/>*/}
+                {/*<Profile  posts={props.state.profilePage.posts}  addPost={props.addPost}/>*/}
                 <Routes>
                     <Route path={'/dialogs'} element={<Dialogs messagesData={props.state.messagesPage.messagesData}
                                                                dialogsData={props.state.messagesPage.dialogsData}/>}/>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,31 +3,19 @@ import classes from "./Nav.module.css";
 import {useNavigate} from "react-router-dom";
 
 const Nav = () => {
-    const navigate = useNavigate();
-    const [activeNav, setActiveNav] = useState('profile')
+
 
     return <nav className={classes.nav}>
-        <div onClick={() => {setActiveNav('profile'); navigate('/profile')}}
-             className={activeNav === 'profile' ? `${classes.item}${classes.activ}` : classes.item}>
-            Profile
-        </div>
-        <div
-            onClick={() => {setActiveNav('dialogs'); navigate('/dialogs')}}
-            className={activeNav === 'dialogs' ? `${classes.item}${classes.activ}` : classes.item}
-        >
-            Messenger
-        </div>
-        <div onClick={() => {setActiveNav('new'); navigate('/new')}}
-            className={activeNav === 'new' ? `${classes.item}${classes.activ}` : classes.item}>
-            <a>New</a>
 
-        </div>
-        <div className={classes.item}>
-            <a>Music</a>
-        </div>
-        <div className={classes.item}>
-            <a>Settings</a>
-        </div>
+        <div className={classes.item}><a>Profile</a></div>
+
+        <div className={classes.item}><a>Messenger</a></div>
+
+        <div className={classes.item}><a>New</a></div>
+
+        <div className={classes.item}><a>Music</a></div>
+
+        <div className={classes.item}><a>Settings</a></div>
     </nav>
 };
 
